18f9fb235b243078fd46637698fb96a5f7643ea4
Andres Soria <andres.zov@gmail.com>
2022-11-22 14:12:38 -0600

    Create function to create a new cafe with a random city and state

--- seeds/index.js
+++ seeds/index.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const cities = require("./cities");
 const Coffeeshop = require("../models/coffeeshop");
 
 mongoose
@@ -13,8 +14,13 @@ mongoose
 
 const seedDB = async () => {
   await Coffeeshop.deleteMany({});
-  const c = new Coffeeshop({ title: "Purple Bean" });
-  await c.save();
+  for (let i = 0; i < 50; i++) {
+    const random1000 = Math.floor(Math.random() * 1000);
+    const cafe = new Coffeeshop({
+      location: `${cities[random1000].city}, ${cities[random1000].state}`,
+    });
+    await cafe.save();
+  }
 };
 
 seedDB();
