cde03b54a997b4f600248e9b1a851e87564fe76e
Andres Soria <andres.zov@gmail.com>
2022-11-22 14:18:58 -0600

    Create function to add a random title for our cafes

--- seeds/index.js
+++ seeds/index.js
@@ -13,12 +13,15 @@ mongoose
     console.log(err);
   });
 
+const sample = (array) => array[Math.floor(Math.random() * array.length)];
+
 const seedDB = async () => {
   await Coffeeshop.deleteMany({});
   for (let i = 0; i < 50; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
     const cafe = new Coffeeshop({
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
+      title: `${sample(descriptors)} ${sample(places)}`,
     });
     await cafe.save();
   }
