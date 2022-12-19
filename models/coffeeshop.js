881afaf8e7b51d896bd10d56d9a5933347ab4555
Andres Soria <andres.zov@gmail.com>
2022-12-19 13:20:25 -0600

    Update coffeeshop model to include and image and an average price

--- models/coffeeshop.js
+++ models/coffeeshop.js
@@ -3,7 +3,8 @@ const Schema = mongoose.Schema;
 
 const CoffeeshopSchema = new Schema({
   title: String,
-  price: String,
+  image: String,
+  avgprice: Number,
   description: String,
   location: String,
 });
--- seeds/index.js
+++ seeds/index.js
@@ -19,9 +19,14 @@ const seedDB = async () => {
   await Coffeeshop.deleteMany({});
   for (let i = 0; i < 50; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
+    const avgprice = Math.floor(Math.random() * 20) + 10;
     const cafe = new Coffeeshop({
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
+      image: "https://source.unsplash.com/collection/2557534",
+      description:
+        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad aut maiores rem, repellendus neque quis inventore dolorem magnam, sunt autem similique corrupti iusto! Modi, earum? Deserunt minus porro fuga atque.",
+      avgprice,
     });
     await cafe.save();
   }
