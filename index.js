ce0f150c403e6a2252b227e2229079daaa942a80
Andres Soria <andres.zov@gmail.com>
2023-01-02 12:13:34 -0600

    Apply custom error wrapper to all async functions

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
 const ejsMate = require("ejs-mate");
+const catchAsync = require("./utilities/catchAsync");
 const methodOverride = require("method-override");
 const Coffeeshop = require("./models/coffeeshop");
 
@@ -28,48 +29,62 @@ app.get("/", (req, res) => {
   res.render("home");
 });
 
-app.get("/coffeeshops", async (req, res) => {
-  const cafes = await Coffeeshop.find({});
-  res.render("coffeeshops/index", { cafes });
-});
+app.get(
+  "/coffeeshops",
+  catchAsync(async (req, res, next) => {
+    const cafes = await Coffeeshop.find({});
+    res.render("coffeeshops/index", { cafes });
+  })
+);
 
 app.get("/coffeeshops/new", (req, res) => {
   res.render("coffeeshops/new");
 });
 
-app.post("/coffeeshops", async (req, res, next) => {
-  try {
+app.post(
+  "/coffeeshops",
+  catchAsync(async (req, res, next) => {
     const cafe = new Coffeeshop(req.body.cafe);
     await cafe.save();
     res.redirect(`/coffeeshops/${cafe._id}`);
-  } catch (e) {
-    next(e);
-  }
-});
+  })
+);
 
-app.get("/coffeeshops/:id", async (req, res) => {
-  const cafe = await Coffeeshop.findById(req.params.id);
-  res.render("coffeeshops/show", { cafe });
-});
+app.get(
+  "/coffeeshops/:id",
+  catchAsync(async (req, res) => {
+    const cafe = await Coffeeshop.findById(req.params.id);
+    res.render("coffeeshops/show", { cafe });
+  })
+);
 
-app.get("/coffeeshops/:id/edit", async (req, res) => {
-  const cafe = await Coffeeshop.findById(req.params.id);
-  res.render("coffeeshops/edit", { cafe });
-});
+app.get(
+  "/coffeeshops/:id/edit",
+  catchAsync(async (req, res) => {
+    const cafe = await Coffeeshop.findById(req.params.id);
+    res.render("coffeeshops/edit", { cafe });
+  })
+);
 
-app.put("/coffeeshops/:id", async (req, res) => {
-  const { id } = req.params;
-  const cafe = await Coffeeshop.findByIdAndUpdate(id, {
-    ...req.body.cafe,
-  });
-  res.redirect(`/coffeeshops/${cafe._id}`);
-});
+app.put(
+  "/coffeeshops/:id",
+  catchAsync(async (req, res) => {
+    const { id } = req.params;
+    const cafe = await Coffeeshop.findByIdAndUpdate(id, {
+      ...req.body.cafe,
+    });
+    res.redirect(`/coffeeshops/${cafe._id}`);
+  })
+);
 
-app.delete("/coffeeshops/:id", async (req, res) => {
-  const { id } = req.params;
-  const cafe = await Coffeeshop.findByIdAndDelete(id);
-  res.redirect("/coffeeshops");
-});
+app.delete(
+  "/coffeeshops/:id",
+  catchAsync(async (req, res) => {
+    const { id } = req.params;
+    const cafe = await Coffeeshop.findByIdAndDelete(id);
+    res.redirect("/coffeeshops");
+  })
+);
 
 app.use((err, req, res, next) => {
   res.send("Something went wrong");
