a9bbcb4b64854d1b4f8e0db175711304a7b6e5bd
Andres Soria <andres.zov@gmail.com>
2023-01-10 13:57:27 -0600

    Fix Joi validation position inside module

--- schemas.js
+++ schemas.js
@@ -3,9 +3,9 @@ const Joi = require("joi");
 module.exports.coffeeshopSchema = Joi.object({
   cafe: Joi.object({
     title: Joi.string().required(),
+    image: Joi.string().required(),
     avgprice: Joi.number().required().min(0).max(100),
+    description: Joi.string().required(),
+    location: Joi.string().required(),
   }).required(),
-  image: Joi.string().required(),
-  location: Joi.string().required(),
-  description: Joi.string().required(),
 });
