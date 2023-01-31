c9a445dfaf471951aad480cd9113eda7b88e9f09
Andres Soria <andres.zov@gmail.com>
2023-01-31 13:34:16 -0600

    Set up flash middleware

--- index.js
+++ index.js
@@ -44,6 +44,12 @@ const sessionConfig = {
 app.use(session(sessionConfig));
 app.use(flash());
 
+app.use((req, res, next) => {
+  res.locals.success = req.flash("success");
+  res.locals.error = req.flash("error");
+  next();
+});
+
 app.use("/coffeeshops", coffeeshops);
 app.use("/coffeeshops/:id/reviews", reviews);
 
