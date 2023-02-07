1dd6007248cd50566b2a3f8dcb6831597dc03f97
Andres Soria <andres.zov@gmail.com>
2023-02-07 15:15:23 -0600

    Create login form and handle authentication with passport on post route

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const catchAsync = require("../utilities/catchAsync");
 const User = require("../models/user");
+const passport = require("passport");
 
 router.get("/register", (req, res) => {
   res.render("auth/register");
@@ -21,6 +22,22 @@ router.post(
       res.redirect("/register");
     }
   })
+);
+
+router.get("/login", (req, res) => {
+  res.render("auth/login");
+});
+
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    failureFlash: true,
+    failureRedirect: "/login",
+  }),
+  (req, res) => {
+    req.flash("success", "Welcome Back!");
+    res.redirect("/coffeeshops");
+  }
 );
 
 module.exports = router;
