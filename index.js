4adac34e55328535ac31688de7a1d2c4c2a173ca
Andres Soria <andres.zov@gmail.com>
2023-02-07 13:14:14 -0600

    Add passport configuration

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const session = require("express-session");
 const flash = require("connect-flash");
 const ExpressError = require("./utilities/ExpressError");
 const methodOverride = require("method-override");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user");
 
 // Routes
 const coffeeshops = require("./routes/coffeeshops");
@@ -41,8 +44,15 @@ const sessionConfig = {
     maxAge: 1000 * 60 * 60 * 24 * 7,
   },
 };
+
 app.use(session(sessionConfig));
 app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.use((req, res, next) => {
   res.locals.success = req.flash("success");
