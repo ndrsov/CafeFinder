ed2f5065a0d5df122ea66ca04bbd2e4c97efc2b6
Andres Soria <andres.zov@gmail.com>
2023-02-07 15:02:22 -0600

    Handle register post route to create user and handle errors

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,26 @@
 const express = require("express");
 const router = express.Router();
+const catchAsync = require("../utilities/catchAsync");
 const User = require("../models/user");
 
 router.get("/register", (req, res) => {
   res.render("auth/register");
 });
 
-router.post("/register", async (req, res) => {
-  res.send(req.body);
-});
+router.post(
+  "/register",
+  catchAsync(async (req, res) => {
+    try {
+      const { email, username, password } = req.body;
+      const user = new User({ email, username });
+      const registeredUser = await User.register(user, password);
+      req.flash("success", "Thanks for registering, Welcome to CafeFinder!");
+      res.redirect("/coffeeshops");
+    } catch (err) {
+      req.flash("error", err.message);
+      res.redirect("/register");
+    }
+  })
+);
 
 module.exports = router;
