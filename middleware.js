3d3474e84e5f52356b703a19a1b07f7fe104c53b
Andres Soria <andres.zov@gmail.com>
2023-02-27 13:32:20 -0600

    Create isReviewAuthor to check ownership of review

--- middleware.js
+++ middleware.js
@@ -29,6 +29,16 @@ module.exports.isAuthor = async (req, res, next) => {
   next();
 };
 
+module.exports.isReviewAuthor = async (req, res, next) => {
+  const { id, reviewId } = req.params;
+  const review = await Review.findById(reviewId);
+  if (!review.author.equals(req.user._id)) {
+    req.flash("error", "You do not have permission to do that!");
+    return res.redirect(`/coffeeshops/${id}`);
+  }
+  next();
+};
+
 module.exports.validateReview = (req, res, next) => {
   const { error } = reviewSchema.validate(req.body);
   if (error) {
