4fe2f9947d4f84eb385ccaa31c1861120403b6ee
Andres Soria <andres.zov@gmail.com>
2023-03-06 15:51:50 -0600

    Add missing params object on cloudinary storage

--- cloudinary/index.js
+++ cloudinary/index.js
@@ -9,8 +9,10 @@ cloudinary.config({
 
 const storage = new CloudinaryStorage({
   cloudinary,
-  folder: 'CafeFinder',
-  allowedFormats: ['jpeg', 'png', 'jpg'],
+  params: {
+    folder: 'CafeFinder',
+    allowedFormats: ['jpeg', 'png', 'jpg'],
+  },
 });
 
 module.exports = {
