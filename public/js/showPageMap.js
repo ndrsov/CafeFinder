ca2494f25af26c7a5fee722759cfb35469fcaf71
Andres Soria <andres.zov@gmail.com>
2023-03-14 17:10:51 -0600

    Add popup to map

--- public/js/showPageMap.js
+++ public/js/showPageMap.js
@@ -2,8 +2,15 @@ mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
   container: 'map', // container ID
   style: 'mapbox://styles/mapbox/streets-v12', // style URL
-  center: cafeLocation, // starting position [lng, lat]
+  center: cafe.geometry.coordinates, // starting position [lng, lat]
   zoom: 14, // starting zoom
 });
 
-const marker1 = new mapboxgl.Marker().setLngLat(cafeLocation).addTo(map);
+const marker1 = new mapboxgl.Marker()
+  .setLngLat(cafe.geometry.coordinates)
+  .setPopup(
+    new mapboxgl.Popup({ offset: 20 }).setHTML(
+      `<h4>${cafe.title}</h4><p>${cafe.location}</p>`
+    )
+  )
+  .addTo(map);
