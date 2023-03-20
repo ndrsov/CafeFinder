240abf0091b65014d282c570d10dca95db9e81bb
Andres Soria <andres.zov@gmail.com>
2023-03-20 14:46:41 -0600

    Add virtuals to create optimized and resize images for thumbails and previews

--- models/coffeeshop.js
+++ models/coffeeshop.js
@@ -11,6 +11,14 @@ ImageSchema.virtual('thumbnail').get(function () {
   return this.url.replace('/upload', '/upload/w_200');
 });
 
+ImageSchema.virtual('hero').get(function () {
+  return this.url.replace('upload/', '/upload/c_fill,h_700,w_1400');
+});
+
+ImageSchema.virtual('grid').get(function () {
+  return this.url.replace('upload/', '/upload/h_350');
+});
+
 const CoffeeshopSchema = new Schema({
   title: String,
   images: [ImageSchema],
